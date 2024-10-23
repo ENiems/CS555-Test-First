c964ef72b90c3e1be7022925f979a648a03da87a
Elliot Niemann <124651877+ENiems@users.noreply.github.com>
2024-10-23 10:09:33 -0400

    added two extra test cases

--- TaskListTest.java
+++ TaskListTest.java
@@ -21,7 +21,7 @@ public class TaskListTest {
         // Verify the task is now marked as complete
         assertTrue(taskList.tasks.get(0).getStatus());
     }
-
+    
     @Test
     public void testEditTask() {
         // Add a task
@@ -69,7 +69,46 @@ public class TaskListTest {
         // Verify the task list is now empty
         assertEquals(0, taskList.tasks.size());
     }
-
+    
+    @Test
+    public void testAddCompleteAndUncompleteTask() {
+        // Add a task and verify it is incomplete by default
+        TaskList taskList = new TaskList();
+        taskList.addTask("Study for exam");
+        // Verify the task is added and the list contains 1 task
+        assertEquals(1, taskList.tasks.size());
+        // Verify the task description
+        assertEquals("Study for exam", taskList.tasks.get(0).getTask());
+        // Verify the task is incomplete by default
+        assertFalse(taskList.tasks.get(0).getStatus());
+        //  Mark the task as complete
+        
+        ////// TASKS IN LIST ARE OFFSET BY 1 ////////////
+        taskList.completeTask(1);
+        // Verify the task is now marked as complete
+        assertTrue(taskList.tasks.get(0).getStatus());
+        
+        // Now, mark task as not complete
+        taskList.uncompleteTask(1);
+        //Verify task is now marked as incomplete
+        assertFalse(taskList.tasks.get(0).getStatus());
+    }
+    
+    @Test
+    public void testEmptyListErrors() {
+    	TaskList taskList = new TaskList();
+    	//Attempt to complete a non-existing task 1
+    	taskList.completeTask(1);
+    	//Check that task list remains empty afterwards
+    	assertEquals(0, taskList.tasks.size());
+    	//Attempt to mark a non-existing task 1 as to do
+    	taskList.uncompleteTask(1);
+    	//Check that task list remains empty afterwards
+    	assertEquals(0, taskList.tasks.size());
+    	//Attempt to edit a non-existing task 1
+    	taskList.editTask(1, "Add an item to my to do list");
+    	//Check that task list remains empty afterwards
+    	assertEquals(0, taskList.tasks.size());
+    }
 
 }
-
